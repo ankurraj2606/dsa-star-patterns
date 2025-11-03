d68b2cf94adb99a78efa6ffcc6322ed7932ef3cb
Ankur Raj <ankurraj2606@gmail.com>
2025-11-03 12:24:02 +0530

    Create hollow-rectangle.java

---
+++ hollow-rectangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+
+        int rows = s.nextInt();
+        int cols = s.nextInt();
+
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= cols; col++) {
+                if (row == 1 || row == rows || col == 1 || col == cols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
